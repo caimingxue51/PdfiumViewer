19390f1f3444deaad4785a4638536cc317933519
caichaoxiong <caichaoxiong@shein.com>
2021-11-03 15:07:08 +0800

    二次改造

---
+++ PdfiumViewer/PdfPrintHard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfiumViewer
+{
+    public class PdfPrintHard
+    {
+        /// <summary>
+        /// Gets to print horizontally.
+        /// </summary>
+        public int HardMarginX { get; }
+
+        /// <summary>
+        /// Gets to print vertically.
+        /// </summary>
+        public int HardMarginY { get; }
+        public PdfPrintHard(int x, int y)
+        {
+            HardMarginX = x;
+            HardMarginY = y;
+        }
+    }
+}
